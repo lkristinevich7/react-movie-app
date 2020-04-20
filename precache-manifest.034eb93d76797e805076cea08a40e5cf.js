90c27b69af4378e53aad058fc553d1d572032cc5
Liudmila Krystsinevich <lkristinevich7@gmail.com>
2020-04-20 12:17:06 +0200

    Updates

---
+++ precache-manifest.034eb93d76797e805076cea08a40e5cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02b752c7e5fae16cd499feffebdc7588",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "ada075521388cc227868",
+    "url": "/react-movie-app/static/css/main.5b9015b6.chunk.css"
+  },
+  {
+    "revision": "5235fca737d39042f61e",
+    "url": "/react-movie-app/static/js/2.215f8639.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-movie-app/static/js/2.215f8639.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ada075521388cc227868",
+    "url": "/react-movie-app/static/js/main.aad0f926.chunk.js"
+  },
+  {
+    "revision": "45a79126a5cdd71eb3ca",
+    "url": "/react-movie-app/static/js/runtime-main.137a6703.js"
+  }
+]);
